0031ad728aad175b11b9560011251c9d2ff022b8
remilebeau <remilebeau90@gmail.com>
2024-03-10 15:04:51 -0400

    removed content for Projects page. Revision coming later

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,15 +1,7 @@
-import React from "react";
-import RepoCard from "../components/RepoCard";
-import getRepos from "../lib/getRepos";
-
 export default async function ProjectPage() {
-  const repos = await getRepos();
   return (
     <main>
       <h1>Projects</h1>
-      {repos.map((repo: any) => (
-        <RepoCard key={repo.name} repo={repo.name} />
-      ))}
     </main>
   );
 }
