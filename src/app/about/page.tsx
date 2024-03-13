ea1a62389a888c851a085fc4e121d88141a145f9
remilebeau <remilebeau90@gmail.com>
2024-03-12 23:13:03 -0400

    First draft of About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,36 +3,52 @@ import Image from "next/image";
 export default function AboutPage() {
   return (
     <main className="max-w-4xl mx-auto">
-      <p className="text-xl m-8">
-        Born in Canada, raised in Columbus, Ohio, USA. I grew up in a Lebanese
-        home: I am first-generation American on my mother's side; and
-        second-generation on my father's side.
-      </p>
-      <hr />
-      <p className="text-xl m-8">
-        I graduated from The Ohio State University with a finance degree in
-        2022.
-      </p>
-
-      <p className="text-xl m-8">
-        After graduating from The Ohio State University in 2020 with a finance
-        degree, I worked for a family friend in wealth management and financial
-        planning.
-      </p>
       <h1 className="text-3xl font-bold text-center">About</h1>
-      <section className="grid grid-cols-4 mt-auto place-items-center gap-4 m-4 p-4">
+      <article
+        id="countries"
+        className="grid grid-cols-2 mt-auto place-items-center gap-8 m-8 p-8"
+      >
+        <Image src="/images/canada.svg" width={400} height={400} alt="Canada" />
+        <Image
+          src="/images/lebanon.svg"
+          width={400}
+          height={400}
+          alt="Lebanon"
+        />
+        <Image src="/images/usa.svg" width={400} height={400} alt="USA" />
+
+        <Image src="/images/france.svg" width={400} height={400} alt="France" />
+        <p className="font-3xl col-span-2 m-8">
+          Born in Canada to Lebanese parents. Grew up and currently living in
+          Columbus, Ohio. Natively fluent in English. Professional working
+          proficiency in French.
+        </p>
+      </article>
+
+      <hr className="border-2 border-white m-8" />
+
+      <article
+        id="university"
+        className="grid grid-cols-2 mt-auto place-items-center gap-8 m-8 p-8"
+      >
         <Image
           src="/images/osu.svg"
           width={400}
           height={400}
           alt="Ohio State Buckeyes"
         />
-        <Image
-          src="./images/vatican.svg"
-          width={400}
-          height={400}
-          alt="Vatican"
-        />
+        <p className="col-span-2">
+          Bachelor of Science in Business Administration, Finance; Summa Cum
+          Laude; 2020
+        </p>
+      </article>
+
+      <hr className="border-2 border-white m-8" />
+
+      <article
+        id="games"
+        className="grid grid-cols-2 mt-auto place-items-center gap-8 m-8 p-8"
+      >
         <Image
           src="/images/nintendo.svg"
           width={400}
@@ -45,17 +61,24 @@ export default function AboutPage() {
           height={400}
           alt="PlayStation"
         />
+        <p className="col-span-2">
+          Avid gamer for over 20 years. Favorite console: Nintendo 64. Favorite
+          game: The Legend of Zelda: Ocarina of Time.
+        </p>
+      </article>
+
+      <hr className="border-2 border-white m-8" />
+
+      <article
+        id="linux"
+        className="grid grid-cols-2 mt-auto place-items-center gap-8 m-8 p-8"
+      >
         <Image src="/images/ubuntu.svg" width={400} height={400} alt="Ubuntu" />
-        <Image src="/images/usa.svg" width={400} height={400} alt="USA" />
-        <Image src="/images/canada.svg" width={400} height={400} alt="Canada" />
-        <Image
-          src="/images/lebanon.svg"
-          width={400}
-          height={400}
-          alt="Lebanon"
-        />
-        <Image src="/images/france.svg" width={400} height={400} alt="France" />
-      </section>
+        <p className="col-span-4">
+          Using Linux desktop since 2021. Advocate of Free and Open Source
+          Software.
+        </p>
+      </article>
     </main>
   );
 }
