ac06f93b016eaef867888da09117fe7b1b861cc1
remilebeau <remilebeau90@gmail.com>
2024-03-13 23:08:11 -0400

    Revised layout and content of About page.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,21 +5,19 @@ export default function AboutPage() {
     <main>
       <h1>About</h1>
       <article id="countries">
-        <Image src="/images/canada.svg" width={400} height={400} alt="Canada" />
         <Image
           src="/images/lebanon.svg"
           width={400}
           height={400}
           alt="Lebanon"
         />
+        <Image src="/images/canada.svg" width={400} height={400} alt="Canada" />
         <Image src="/images/usa.svg" width={400} height={400} alt="USA" />
-
         <Image src="/images/france.svg" width={400} height={400} alt="France" />
-        <p className="col-span-2">
-          Born in Canada to Lebanese parents. Grew up and currently living in
-          Columbus, Ohio. Natively fluent in English. Professional working
-          proficiency in French.
-        </p>
+        <p className="row-start-2">Lebanese ethnicity</p>
+        <p className="row-start-2">Born in Canada</p>
+        <p className="row-start-4">Living in Columbus, OH</p>
+        <p className="row-start-4">Additional languages: French</p>
       </article>
 
       <hr />
@@ -54,9 +52,10 @@ export default function AboutPage() {
           alt="PlayStation"
         />
         <p className="col-span-2">
-          Avid gamer for over 20 years <br />
+          Gaming for over 20 years <br />
           Favorite console: Nintendo 64 <br />
-          Favorite game: The Legend of Zelda: Ocarina of Time
+          Favorite game: <br />
+          The Legend of Zelda: Ocarina of Time
         </p>
       </article>
 
@@ -71,8 +70,8 @@ export default function AboutPage() {
         />
         <Image src="/images/ubuntu.svg" width={400} height={400} alt="Ubuntu" />
         <p className="col-span-2">
-          Full-time Linux desktop user since 2021. <br />
-          Advocate of Free and Open Source Software.
+          Full-time Linux desktop user since 2021 <br />
+          Advocate of Free and Open Source Software
         </p>
       </article>
     </main>
