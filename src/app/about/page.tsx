b50c3010383eb4cc841ed9c4941d860c7dfdc40b
remilebeau <remilebeau90@gmail.com>
2024-03-15 21:51:20 -0400

    restyling about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,94 +1,117 @@
 import Image from "next/image";
 
 export default function AboutPage() {
+  const imageClasses = "mb-6";
   return (
     <main>
       <h2>About</h2>
       <section>
         <article id="countries">
-          <Image
-            src="/images/lebanon.svg"
-            width={400}
-            height={400}
-            alt="Lebanon"
-          />
-          <Image
-            src="/images/canada.svg"
-            width={400}
-            height={400}
-            alt="Canada"
-          />
-          <Image src="/images/usa.svg" width={400} height={400} alt="USA" />
-          <Image
-            src="/images/france.svg"
-            width={400}
-            height={400}
-            alt="France"
-          />
-          <p>Lebanese ethnicity</p>
-          <p>Born in Canada</p>
-          <p>Living in Columbus, OH</p>
-          <p>Additional languages: French</p>
+          <ul>
+            <li>
+              <Image
+                src="/images/lebanon.svg"
+                width={400}
+                height={400}
+                alt="Lebanon"
+                className={imageClasses}
+              />
+              <p>Lebanese ethnicity</p>
+            </li>
+            <li>
+              <Image
+                src="/images/canada.svg"
+                width={400}
+                height={400}
+                alt="Canada"
+                className={imageClasses}
+              />
+              <p>Born in Canada</p>
+            </li>
+            <li>
+              <Image
+                src="/images/usa.svg"
+                width={400}
+                height={400}
+                alt="USA"
+                className={imageClasses}
+              />
+              <p>Raised and living in Columbus, OH</p>
+            </li>
+            <li>
+              <Image
+                src="/images/france.svg"
+                width={400}
+                height={400}
+                alt="France"
+                className={imageClasses}
+              />
+              <p>Additional languages: French</p>
+            </li>
+          </ul>
         </article>
 
         <hr />
 
         <article id="university">
-          <Image
-            src="/images/osu.svg"
-            width={400}
-            height={400}
-            alt="Ohio State Buckeyes"
-          />
-          <p className="col-start-2">
-            BSBA <br />
-            Summa Cum Laude <br />
-            2020
-          </p>
+          <ul>
+            <li>
+              <Image
+                src="/images/osu.svg"
+                width={400}
+                height={400}
+                alt="Ohio State Buckeyes"
+                className={imageClasses}
+              />
+              <p>
+                BSBA <br />
+                Summa Cum Laude <br />
+                2020
+              </p>
+            </li>
+          </ul>
         </article>
 
         <hr />
 
         <article id="games">
-          <Image
-            src="/images/nintendo.svg"
-            width={400}
-            height={400}
-            alt="Nintendo"
-          />
-          <Image
-            src="/images/playstation.svg"
-            width={400}
-            height={400}
-            alt="PlayStation"
-          />
-          <p className="col-span-2 ">
-            Gaming for over 20 years <br />
-            Favorite console: Nintendo 64 <br />
-            Favorite game: <br />
-            The Legend of Zelda: Ocarina of Time
-          </p>
+          <ul>
+            <li>
+              <Image
+                src="/images/nintendo.svg"
+                width={400}
+                height={400}
+                alt="Nintendo"
+                className={imageClasses}
+              />
+              <p>
+                Favorite hobby: Gaming <br />
+                Favorite console: Nintendo 64 <br />
+                Favorite game: <br />
+                The Legend of Zelda: Ocarina of Time
+              </p>
+            </li>
+          </ul>
         </article>
 
         <hr />
 
         <article id="linux">
-          <Image
-            src="./images/debian.svg"
-            width={400}
-            height={400}
-            alt="Debian"
-          />
-          <Image
-            src="/images/ubuntu.svg"
-            width={400}
-            height={400}
-            alt="Ubuntu"
-          />
-          <p className="col-span-2">
-            Full-time Linux desktop user since 2021 <br />
-            Advocate of Free and Open Source Software
-          </p>
+          <ul>
+            <li>
+              <Image
+                src="/images/ubuntu.svg"
+                width={400}
+                height={400}
+                alt="Ubuntu"
+                className={imageClasses}
+              />
+              <p>
+                Using Linux desktop since 2021 <br />
+                Advocate of Free and Open Source Software
+              </p>
+            </li>
+          </ul>
         </article>
       </section>
     </main>
