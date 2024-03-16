922e689f32de98f2e1498be391039df7ad2c165c
remilebeau <remilebeau90@gmail.com>
2024-03-16 14:09:32 -0400

    added styling and content for footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,11 +1,19 @@
 export default function Footer() {
   return (
     <footer>
-      {/* <section className="footer-container">
-        <a href="mailto:remilebeau90@gmail.com">
-          <p className="font-bold hover:opacity-90">remilebeau90@gmail.com</p>
-        </a>
-      </section> */}
+      <section className="footer-container">
+        <nav aria-label="footer">
+          <a href="mailto:remilebeau90@gmail.com">
+            <p className="font-bold hover:opacity-90">remilebeau90@gmail.com</p>
+          </a>
+        </nav>
+        <div className="flex flex-col sm:gap-2">
+          <p className="text-right">
+            Copyright &copy; <span id="year">2024</span>
+          </p>
+          <p className="text-right">All Rights Reserved</p>
+        </div>
+      </section>
     </footer>
   );
 }
