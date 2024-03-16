cdeeb01a6708e7a1ae844baa87ed72edb77d784d
remilebeau <remilebeau90@gmail.com>
2024-03-16 15:23:14 -0400

    added mobile-menu to header

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,114 +5,112 @@ export default function AboutPage() {
   return (
     <main>
       <h2>About</h2>
-      <section>
-        <article id="countries">
-          <ul>
-            <li>
-              <Image
-                src="/images/lebanon.svg"
-                width={400}
-                height={400}
-                alt="Lebanon"
-                className={imageClasses}
-              />
-              <p>Lebanese ethnicity</p>
-            </li>
-            <li>
-              <Image
-                src="/images/canada.svg"
-                width={400}
-                height={400}
-                alt="Canada"
-                className={imageClasses}
-              />
-              <p>Born in Canada</p>
-            </li>
-            <li>
-              <Image
-                src="/images/usa.svg"
-                width={400}
-                height={400}
-                alt="USA"
-                className={imageClasses}
-              />
-              <p>Raised and living in Columbus, OH</p>
-            </li>
-            <li>
-              <Image
-                src="/images/france.svg"
-                width={400}
-                height={400}
-                alt="France"
-                className={imageClasses}
-              />
-              <p>Additional languages: French</p>
-            </li>
-          </ul>
-        </article>
+      <section id="countries">
+        <ul>
+          <li>
+            <Image
+              src="/images/lebanon.svg"
+              width={400}
+              height={400}
+              alt="Lebanon"
+              className={imageClasses}
+            />
+            <p>Lebanese ethnicity</p>
+          </li>
+          <li>
+            <Image
+              src="/images/canada.svg"
+              width={400}
+              height={400}
+              alt="Canada"
+              className={imageClasses}
+            />
+            <p>Born in Canada</p>
+          </li>
+          <li>
+            <Image
+              src="/images/usa.svg"
+              width={400}
+              height={400}
+              alt="USA"
+              className={imageClasses}
+            />
+            <p>Raised and living in Columbus, OH</p>
+          </li>
+          <li>
+            <Image
+              src="/images/france.svg"
+              width={400}
+              height={400}
+              alt="France"
+              className={imageClasses}
+            />
+            <p>Additional languages: French</p>
+          </li>
+        </ul>
+      </section>
 
-        <hr />
+      <hr />
 
-        <article id="university">
-          <ul>
-            <li>
-              <Image
-                src="/images/osu.svg"
-                width={400}
-                height={400}
-                alt="Ohio State Buckeyes"
-                className={imageClasses}
-              />
-              <p>
-                BSBA <br />
-                Summa Cum Laude <br />
-                2020
-              </p>
-            </li>
-          </ul>
-        </article>
+      <section id="university">
+        <ul>
+          <li>
+            <Image
+              src="/images/osu.svg"
+              width={400}
+              height={400}
+              alt="Ohio State Buckeyes"
+              className={imageClasses}
+            />
+            <p>
+              BSBA <br />
+              Summa Cum Laude <br />
+              2020
+            </p>
+          </li>
+        </ul>
+      </section>
 
-        <hr />
+      <hr />
 
-        <article id="games">
-          <ul>
-            <li>
-              <Image
-                src="/images/nintendo.svg"
-                width={400}
-                height={400}
-                alt="Nintendo"
-                className={imageClasses}
-              />
-              <p>
-                Favorite hobby: Gaming <br />
-                Favorite console: Nintendo 64 <br />
-                Favorite game: <br />
-                The Legend of Zelda: Ocarina of Time
-              </p>
-            </li>
-          </ul>
-        </article>
+      <section id="games">
+        <ul>
+          <li>
+            <Image
+              src="/images/nintendo.svg"
+              width={400}
+              height={400}
+              alt="Nintendo"
+              className={imageClasses}
+            />
+            <p>
+              Favorite hobby: Gaming <br />
+              Favorite console: Nintendo 64 <br />
+              Favorite game: <br />
+              The Legend of Zelda: Ocarina of Time
+            </p>
+          </li>
+        </ul>
+      </section>
 
-        <hr />
+      <hr />
 
-        <article id="linux">
-          <ul>
-            <li>
-              <Image
-                src="/images/ubuntu.svg"
-                width={400}
-                height={400}
-                alt="Ubuntu"
-                className={imageClasses}
-              />
-              <p>
-                Using Linux desktop since 2021 <br />
-                Advocate of Free and Open Source Software
-              </p>
-            </li>
-          </ul>
-        </article>
+      <section id="linux">
+        <ul>
+          <li>
+            <Image
+              src="/images/ubuntu.svg"
+              width={400}
+              height={400}
+              alt="Ubuntu"
+              className={imageClasses}
+            />
+            <p>
+              Using Linux desktop since 2021 <br />
+              Advocate of Free and Open Source Software
+            </p>
+          </li>
+        </ul>
       </section>
     </main>
   );
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,15 +1,15 @@
 "use client";
 import Link from "next/link";
 import { FaGithub, FaEnvelope } from "react-icons/fa";
+import { useState } from "react";
 
 export default function Header() {
-  const onMobileOpenButtonClicked = () => {
-    const mobileOpenButton = document.getElementById("mobile-open-button");
-    const mobileMenu = document.getElementById("mobile-menu");
-    if (mobileOpenButton && mobileMenu) {
-      mobileOpenButton.classList.toggle("hidden");
-      mobileMenu.classList.toggle("hidden");
-    }
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const onMobileMenuButtonClicked = () => {
+    setMobileMenu(!mobileMenu);
+  };
+  const onLinkClicked = () => {
+    setMobileMenu(false);
   };
   const links = [
     { name: "Home", path: "/" },
@@ -17,13 +17,43 @@ export default function Header() {
     { name: "Technologies", path: "/technologies" },
     { name: "Projects", path: "/projects" },
   ];
-  return (
-    <header>
-      <section className="header-container">
-        <h1>Remi Lebeau</h1>
-        <div>
+
+  let content;
+
+  // dropdown mobileMenu enabled
+  if (mobileMenu) {
+    content = (
+      <section className="mobile-menu">
+        <button
+          onClick={onMobileMenuButtonClicked}
+          id="mobile-open-button"
+          className="mobile-menu__btn"
+        >
+          &times;
+        </button>
+        <ul className="mobile-menu__ul">
+          {links.map((link) => (
+            <li
+              key={link.path}
+              onClick={onLinkClicked}
+              className="mobile-menu__li"
+            >
+              <Link href={link.path}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
+  // dropdown mobileMenu disabled
+  // disabled by clicking a Link or pressing the X button
+  else
+    content = (
+      <header>
+        <section className="header-container">
+          <h1>Remi Lebeau</h1>
           <button
-            onClick={onMobileOpenButtonClicked}
+            onClick={onMobileMenuButtonClicked}
             id="mobile-open-button"
             className="text-3xl sm:hidden focus:outline-none"
           >
@@ -36,8 +66,9 @@ export default function Header() {
               </Link>
             ))}
           </nav>
-        </div>
-      </section>
-    </header>
-  );
+        </section>
+      </header>
+    );
+
+  return content;
 }
