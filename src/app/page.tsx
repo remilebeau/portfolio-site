bc36fd611f38802c6070a3e0e81f12d382de0a31
remilebeau <remilebeau90@gmail.com>
2024-03-18 13:13:07 -0400

    removed bio section from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,60 +5,7 @@ export default function HomePage() {
       <section id="welcome">
         <p>
           I&apos;m an aspiring software developer with a financial background
-          seeking to change careers from finance into software development.
-          Currently training in web development but also open to other areas of
-          software development.
-        </p>
-      </section>
-
-      <hr />
-
-      <section id="bio">
-        <p>
-          My first serious experience with programming was a Java class I took
-          in my junior year of high school. Like many programmers, the impostor
-          syndrome was very real. Nevertheless, I entered university studying
-          computer science.
-        </p>
-
-        <p>
-          But during the second semester of my first year, I walked into my
-          computer science class and saw no computers in the room. Instead, the
-          professor took a dry erase marker and wrote out mathematical proofs on
-          the whiteboard. The next day, I switched from computer science to
-          business.
-        </p>
-
-        <p>
-          In 2020, after graduating from The Ohio State University with a
-          business degree, I worked as an admin and bookkeeper for a small
-          wealth management and financial planning firm.
-        </p>
-
-        <p>
-          In 2021, my brother introduced me to Linux desktop, at first as an
-          amusing hobby to use alongside Windows. My first distro was Linux
-          Mint, and as time passed I found myself booting into Linux more than
-          Windows. Before long, I only booted into Windows for PC gaming.
-        </p>
-
-        <p>
-          Using Linux desktop sharpened two skills: problem-solving, and
-          familiarity with a Bash terminal. Two skills I would later learn were
-          critical for a web developer. Using Linux instead of Windows also
-          solved many of the frustrations I had in the past while programming. I
-          admired Linux&apos;s ingenious design, and its history alongside GNU
-          and the Free Software Foundation. Put simply, Linux desktop made me
-          like programming and computing more than I had in a long time.
-        </p>
-
-        <p>
-          Software engineering better suits my personality than finance, and I
-          always preferred tangible skills to salesmanship or consulting. After
-          2 years of Linux desktop, and quitting PC gaming, my final barrier to
-          Linux, in 2023, here I am back to my original plan: software
-          developer. But this time I will focus on web development, and there
-          will be computers...Linux ones preferably.
+          seeking to change careers from finance into web development.
         </p>
       </section>
     </main>
