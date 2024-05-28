d843dbc1c3cf9c93fc5fd49f2b33f918d90cd49f
remilebeau <remilebeau90@gmail.com>
2024-05-28 14:09:16 -0400

    updated Brief History content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -82,21 +82,17 @@ export default function AboutPage() {
       <h2 className="text-center text-5xl font-bold">A Brief History</h2>
       <article className="flex flex-col gap-8">
         <p className="text-2xl">
-          Growing up in the 2000s, two of my hobbies were video games and
-          tinkering on my PC. Video games are still my favorite. Mostly
-          Nintendo, with a period of PC gaming during the Wii and Wii U eras.
-        </p>
-        <p className="text-2xl">
-          Unsure what I wanted to do as a career, I took a Java class in my
-          junior year of high school. While often frustrating, I was drawn to
-          the concept of solving problems using software. After high school, I
-          enrolled at the Ohio State University, majoring in Computer Science.
+          With a long history of gaming and computing as hobbies, and unsure
+          what career path to take, I enrolled in a Java class my junior year of
+          high school. While often frustrating, I was drawn to the idea of
+          solving problems using software. After high school, I enrolled at the
+          Ohio State University, majoring in Computer Science.
         </p>
         <p className="text-2xl">
           I walked into my first computer science class, and saw no computers in
           the room. Instead, the professor grabbed a dry-erase marker and wrote
-          mathematical proofs on the whiteboard. The next day, I switched to the
-          business school.
+          mathematical proofs on the whiteboard for the entire session. The next
+          day, I switched to the Fisher College of Business.
         </p>
         <p className="text-2xl">
           I received my BSBA from OSU in 2020, and began working in financial
@@ -106,16 +102,14 @@ export default function AboutPage() {
           In 2021, my brother introduced me to Linux desktop as a hobby. In my
           tinkering with Linux, I learned about the GNU Project, the Free
           Software Movement, and Linux's overwhelming popularity on servers. I
-          also discovered that much of my previous frustration in programming
-          could be attributed to the quirks of programming on Windows, not
-          necessarily to programming in general. It wasn't long before I
-          switched to Linux full-time for my personal computing, but when I
-          learned about its professional applications, I thought back to my
-          original career path.
+          also discovered that much of my frustration in programming were
+          because of the quirks of programming on Windows, not necessarily
+          programming in general. It wasn't long before I switched to Linux
+          full-time for my personal computing, but when I learned about its
+          professional applications, I thought back to my original career path.
         </p>
         <p className="text-2xl">
-          Thus, in 2023, I began changing careers back toward software
-          engineering. But this time with computers.
+          Thus, in 2023, I began changing careers to full-stack web development.
         </p>
       </article>
     </>
