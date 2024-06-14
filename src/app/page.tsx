e4f75b69513005ef7e8f98da8f9f4d081fa3d6db
remilebeau <remilebeau90@gmail.com>
2024-06-14 16:02:50 -0400

    Changed color theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import AboutCards from "@/components/AboutCards";
 import FavoriteCards from "@/components/FavoriteCards";
 
+//  <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="mx-auto flex max-w-4xl flex-col gap-4 p-8">
       <section className="flex flex-col gap-4">
         <h1 className="text-5xl font-bold">Hello and welcome. I&apos;m Wes.</h1>
         <p className="text-3xl">
@@ -13,18 +15,18 @@ export default function HomePage() {
         <p className="text-3xl">
           Currently focusing on full-stack web development.
         </p>
+        <p className="text-3xl">
+          Please use this site to demo my projects, visit my GitHub, or send me
+          an email.
+        </p>
       </section>
 
-      <p className="text-3xl">
-        Please use this site to demo my projects, visit my GitHub, or send me an
-        email.
-      </p>
       <h2 className="text-center text-4xl font-bold">About</h2>
-      <section className="flex flex-col gap-4 p-4 sm:grid sm:grid-cols-2">
+      <section className="flex flex-col gap-4 sm:grid sm:grid-cols-2">
         <AboutCards />
       </section>
       <h2 className="text-center text-4xl font-bold">Favorites</h2>
-      <section className="flex flex-col gap-4 p-4 sm:grid sm:grid-cols-2">
+      <section className="flex flex-col gap-4 sm:grid sm:grid-cols-2">
         <FavoriteCards />
       </section>
     </main>
