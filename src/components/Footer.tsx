4b1b5b06e95c8ef3866a086991cd21394d28b5b2
remilebeau <remilebeau90@gmail.com>
2024-06-17 14:40:58 -0400

    Added hover tooltip to Footer icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,27 +1,64 @@
 import { FaEnvelope, FaGithub } from "react-icons/fa";
 import { FiFileText } from "react-icons/fi";
 import { ModeToggle } from "./ThemeSwitch";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
 export default function Footer() {
   return (
     <footer className="bg-primary">
       <section className="mx-auto flex max-w-4xl flex-row items-center justify-evenly p-4 font-bold">
-        <a
-          href="https://github.com/remilebeau"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaGithub className="text-5xl" />
-        </a>
-        <a
-          href="mailto:remilebeau90@gmail.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaEnvelope className="text-5xl" />
-        </a>
-        <a href="/wes-s-resume.pdf" download>
-          <FiFileText className="text-5xl" />
-        </a>
+        {/* GitHub */}
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <a
+                href="https://github.com/remilebeau"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaGithub className="text-5xl hover:opacity-80" />
+              </a>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>GitHub</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        {/* Email */}
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <a
+                href="mailto:remilebeau90@gmail.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaEnvelope className="text-5xl hover:opacity-80" />
+              </a>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Email</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        {/* Resume */}
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <a href="/wes-s-resume.pdf" download>
+                <FiFileText className="text-5xl hover:opacity-80" />
+              </a>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Download Resume</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
         <ModeToggle />
       </section>
     </footer>
