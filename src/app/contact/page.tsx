bcd1ee60ab8d652bbfb8544de7be962c659eeef8
remilebeau <remilebeau90@gmail.com>
2024-07-10 15:38:03 -0400

    update contact page content

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,26 +1,22 @@
 export default function ContactPage() {
   return (
-    <main className="mx-auto flex max-w-4xl flex-col gap-8 p-8">
+    <main className="mx-auto flex max-w-4xl flex-col gap-4 p-4">
       <h1 className="text-center text-5xl font-bold text-primary">
         Want To...
       </h1>
-      <article>
-        <h2 className="text-3xl font-bold text-primary">
-          Offer a Job Opportunity?
-        </h2>
-        <p className="text-2xl">
-          I am open to discussing potential job opportunities or collaborations.
-        </p>
-      </article>
-      <article>
-        <h2 className="text-3xl font-bold text-primary">Connect?</h2>
-        <p className="text-2xl">
-          Networking is key in the tech industry, and I&apos;m always looking to
-          meet new people and expand my professional circle. Whether you&apos;re
-          a fellow developer, designer, or an entrepeneur, I&apos;d love to chat
-          and learn more about your work.
-        </p>
-      </article>
+
+      <h2 className="text-3xl font-bold text-primary">Collaborate?</h2>
+      <p className="text-2xl">
+        I am open to discussing potential job opportunities or collaborations.
+      </p>
+
+      <h2 className="text-3xl font-bold text-primary">Connect?</h2>
+      <p className="text-2xl">
+        I&apos;m always looking to meet new people and expand my professional
+        circle. Whatever your background - software engineering, finance, or
+        business analytics - I&apos;d love to chat and learn more about your
+        work.
+      </p>
     </main>
   );
 }
