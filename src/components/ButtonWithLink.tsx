3087df11711fe925ff8108edd6366a9a809a91b9
remilebeau <remilebeau90@gmail.com>
2024-08-10 23:36:34 -0400

    set links to open in new tab

--- src/components/ButtonWithLink.tsx
+++ src/components/ButtonWithLink.tsx
@@ -6,7 +6,9 @@ type Props = {
 export default function ButtonWithLink({ href, text }: Props) {
   return (
     <button className="rounded-xl bg-primary p-2 text-2xl font-bold text-secondary hover:opacity-80">
-      <a href={href}>{text}</a>
+      <a target="_blank" rel="noreferrer" href={href}>
+        {text}
+      </a>
     </button>
   );
 }
