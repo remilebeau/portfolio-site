db597567ccc41f2061c2963d92a57379346ff698
remilebeau <remilebeau90@gmail.com>
2024-09-02 15:50:21 -0400

    add nestjs and mongodb cards

--- src/components/TechnologyCards.tsx
+++ src/components/TechnologyCards.tsx
@@ -3,6 +3,8 @@ import js from "../../public/images/js.svg";
 import ts from "../../public/images/ts.svg";
 import python from "../../public/images/python.svg";
 import nodejs from "../../public/images/nodejs.svg";
+import nestjs from "../../public/images/nestjs.svg";
+import mongodb from "../../public/images/mongodb.svg";
 import react from "../../public/images/react.svg";
 import nextjs from "../../public/images/nextjs.svg";
 import tailwind from "../../public/images/tailwind.svg";
@@ -49,6 +51,15 @@ export default function TechnologyCards() {
       title: "Node.js",
       src: nodejs,
     },
+    {
+      title: "NestJS",
+      src: nestjs,
+    },
+    {
+      title: "MongoDB",
+      src: mongodb,
+    },
+
     {
       title: "Django",
       src: django,
