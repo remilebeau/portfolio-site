d3c4b5c4a4f6fa904a6e303d8ea099d88bb9dc39
remilebeau <remilebeau90@gmail.com>
2024-09-12 15:55:13 -0400

    move class names

--- src/components/ButtonWithLink.tsx
+++ src/components/ButtonWithLink.tsx
@@ -7,13 +7,8 @@ type Props = {
 
 export default function ButtonWithLink({ href, text }: Props) {
   return (
-    <Button>
-      <a
-        className="font-bold sm:text-2xl"
-        target="_blank"
-        rel="noreferrer"
-        href={href}
-      >
+    <Button className="font-bold sm:text-2xl">
+      <a target="_blank" rel="noreferrer" href={href}>
         {text}
       </a>
     </Button>
