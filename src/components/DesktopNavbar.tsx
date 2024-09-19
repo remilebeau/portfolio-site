b8ad2ffe44eb5c2a2e5eeb2be974cbf1f40f332b
remilebeau <remilebeau90@gmail.com>
2024-09-19 09:49:52 -0500

    remove custom coloring to begin replacing with shadcn

--- src/components/DesktopNavbar.tsx
+++ src/components/DesktopNavbar.tsx
@@ -11,7 +11,7 @@ export default function DesktopNavbar({ links }: Props) {
     </Link>
   ));
   return (
-    <nav className="mx-auto flex max-w-4xl flex-row justify-evenly p-4 text-3xl font-bold text-secondary">
+    <nav className="mx-auto flex max-w-4xl flex-row justify-evenly p-4 text-3xl font-bold">
       {renderedLinks}
     </nav>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { ModeToggle } from "./ThemeSwitch";
 
 export default function Footer() {
   return (
-    <footer className="rounded-t-xl bg-primary">
+    <footer className="rounded-t-xl bg-card">
       <section className="mx-auto flex max-w-4xl flex-row items-center justify-evenly p-4 font-bold">
         {/* Dark Mode Toggle */}
         <ModeToggle />
