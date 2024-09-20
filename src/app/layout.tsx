1edf5bfd9bef3e67bfd47d88233abe14a6e18e2c
remilebeau <remilebeau90@gmail.com>
2024-09-20 10:00:13 -0500

    add scroll-smooth behavior to html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body className={`bg-slate-900 text-white ${roboto.className}`}>
         <Header />
         {children}
